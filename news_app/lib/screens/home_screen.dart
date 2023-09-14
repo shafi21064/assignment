3033a4dd17bc0ca1fcf0b824688215dfdd789dbf
Md Shadiku Islam Shafi <shadikulshafi@yahoo.com>
2023-09-15 00:27:58 +0600

    'News_app_created'

---
+++ news_app/lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('News App'),
+        centerTitle: true,
+      ),
+
+    );
+  }
+}
